4e42ba0401a4369b52f7cc211af4aeca6900984e
denizbora <denizbora@protonmail.com>
2021-02-11 20:57:11 +0300

    Update

--- Twitify/Data/TwitifyController.cs
+++ Twitify/Data/TwitifyController.cs
@@ -12,7 +12,7 @@ namespace Twitify.Data
     public class TwitifyController : Controller
     {
         public static readonly IAuthenticationRequestStore _myAuthRequestStore = new LocalAuthenticationRequestStore();
-        public static string ConsumerKey = "";// get from developer.twitter.com, needs developer account
+        public static string ConsumerKey = ""; // get from developer.twitter.com, needs developer account
         public static string ConsumerSecret = "";// get from developer.twitter.com, needs developer account
         public static string TwitterUri = "";// write your twitter callback url
         public static string clientId = "";// get from developer.spotify.com
